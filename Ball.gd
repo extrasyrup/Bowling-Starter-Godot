140a156e1739c68a2c7b69d124d38ca44d4d1549
Extra Syrup <udoobu@gmail.com>
2020-05-14 11:15:42 -0400

    Improvements
    Direction and rotation iimprovements. Ball reacts better to the position clicked.

--- Ball.gd
+++ Ball.gd
@@ -11,11 +11,11 @@ func ball_jump():
 	
 func ball_left():
 	print ("Ball Left")
-	global_translate(Vector3(0, 0, -.2)) #Shift ball position left
+	global_translate(Vector3(0, 0, -.4)) #Shift ball position left
 
 func ball_right():
 	print ("Ball Right")
-	global_translate(Vector3(0, 0, .2)) #Shift ball position right
+	global_translate(Vector3(0, 0, .4)) #Shift ball position right
 
 func ball_rot_left():
 	print ("Ball Left")
@@ -27,20 +27,17 @@ func ball_rot_right():
 
 func ball_push():
 	print ("Ball Push")
+	push_ball(Vector3(0, .1, 0))
+
+func _on_Ball_input_event(camera, event, click_position, click_normal, shape_idx):
+	if event is InputEventMouseButton and !event.pressed:
+		print ("Ball Click Push")
+		push_ball(click_normal)
+
+func push_ball(location):
 	get_node("Particles_Direction").hide()
 	get_node("Particles_Direction").emitting = false
 	mode = RigidBody.MODE_RIGID #Ball is initially set to STATIC in Main.gd so that it can be positioned before push
-	apply_impulse(Vector3(0, 0, 0), Vector3(-6, 5, 0)) #Simulating a pull-back with physics
-	print(get_global_transform().basis)
+	apply_impulse(Vector3(0, -.1, 0), Vector3(-6, 5, 0)) #Simulating a pull-back with physics
 	yield(get_tree().create_timer(0.4), "timeout") #Wait timer to give pull-back effect a moment
-	apply_impulse(Vector3(0, .1, 0), get_global_transform().basis.z * 80) #Impulse to push the ball in facing direction
-
-func _on_Ball_input_event(camera, event, click_position, click_normal, shape_idx):
-	if event is InputEventMouseButton:
-		print ("Ball Click Push")
-		get_node("Particles_Direction").hide()
-		get_node("Particles_Direction").emitting = false
-		mode = RigidBody.MODE_RIGID #Ball is initially set to STATIC in Main.gd so that it can be positioned before push
-		apply_impulse(Vector3(0, -.1, 0), Vector3(-6, 5, 0)) #Simulating a pull-back with physics
-		yield(get_tree().create_timer(0.4), "timeout") #Wait timer to give pull-back effect a moment
-		apply_impulse(click_normal, Vector3(20, 0, 0)) #Impulse to push the ball forward at position you clicked
+	apply_impulse(location, get_global_transform().basis.z * 80) #Impulse to push the ball forward at position you clicked
--- Models/Ball - Mercury.gd
+++ Models/Ball - Mercury.gd
@@ -1,16 +1,16 @@
-extends Spatial
+extends RigidBody
 
-
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
-
-# Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	global_rotate(Vector3(0, 1, 0), .02)
+	if MODE_RIGID:
+		global_rotate(Vector3(0, 1, 0), .02)
+
+func _on_Ball__Mercury_input_event(camera, event, click_position, click_normal, shape_idx):
+	if event is InputEventMouseButton and !event.pressed:
+		print("Mercury clicked.")
+		mode = RigidBody.MODE_RIGID #Ball is initially set to STATIC in Main.gd so that it can be positioned before push
+		apply_impulse(Vector3(0, -.1, 0), Vector3(-6, 5, 0)) #Simulating a pull-back with physics
+		yield(get_tree().create_timer(0.4), "timeout") #Wait timer to give pull-back effect a moment
+		apply_impulse(click_normal, Vector3(200, 40, 0)) #Impulse to push the ball forward at position you clicked
